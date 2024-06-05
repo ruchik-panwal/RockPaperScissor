6bfddc9096b6186a022683d1002cd5e7b3140581
Ruchik <ruchik.panwal@gmail.com>
2024-06-06 00:37:17 +0530

    99.9% done

--- javascript.js
+++ javascript.js
@@ -21,7 +21,20 @@ const final = document.querySelector("#final");
 const Cscore = document.querySelector("#Cscore");
 const Hscore = document.querySelector("#Hscore");
 
+const humanUi = document.querySelector("#humanUi");
+const computerUi = document.querySelector("#computerUi");
 
+const rockIco = document.createElement("i");
+rockIco.classList.add("fa-regular", "fa-hand-back-fist");
+rockIco.id = 'symUi';
+
+const paperIco = document.createElement("i");
+paperIco.classList.add("fa-regular", "fa-hand");
+paperIco.id = 'symUi';
+
+const scissorsIco = document.createElement("i");
+scissorsIco.classList.add("fa-regular", "fa-hand-scissors");
+scissorsIco.id = 'symUi';
 
 
 btns.forEach((button) => {
@@ -32,6 +45,14 @@ btns.forEach((button) => {
 
         const compChoice = getComputerChoice();
 
+        if (humanUi.hasChildNodes()) {
+            humanUi.removeChild(humanUi.firstElementChild);
+        }
+
+        if (computerUi.hasChildNodes()) {
+            computerUi.removeChild(computerUi.firstElementChild);
+        }
+
         if (button.id == "Reset") {
             round.textContent = "S T A R T";
             humanScore = 0;
@@ -41,13 +62,14 @@ btns.forEach((button) => {
 
         else if (button.id == "Rock") {
             round.textContent = playRound("rock", compChoice);
+
         }
-        else if (button.id == "Paper")
+        else if (button.id == "Paper") {
             round.textContent = playRound("paper", compChoice);
-
-        else
+        }
+        else {
             round.textContent = playRound("scissors", compChoice);
-
+        }
 
         if (humanScore > 4) {
 
@@ -59,63 +81,78 @@ btns.forEach((button) => {
             round.textContent = "You Lost!";
         }
 
-
-
         Hscore.textContent = "PLAYER = " + humanScore;
         Cscore.textContent = "COMPUTER = " + computerScore;
-
-
 
     })
 })
 
 
 
-
-
-
-
-
 function playRound(humanChoice, computerChoice) {
 
-    console.log("human = " + humanChoice)
-    console.log("computer = " + computerChoice)
-
-    if (humanChoice == computerChoice)
+    if (humanChoice == computerChoice){
+        
+        if(humanChoice == "rock"){
+            humanUi.appendChild(rockIco);
+            computerUi.appendChild(rockIco);
+        }
+        else if(humanChoice == "paper"){
+            humanUi.appendChild(paperIco);
+            computerUi.appendChild(paperIco);
+        }
+        else{
+            humanUi.appendChild(scissorsIco);
+            computerUi.appendChild(scissorsIco);
+        }
         return "It's a tie"
+    }
 
     if (humanChoice == "rock") {
 
+        humanUi.appendChild(rockIco);
+
         if (computerChoice == "scissors") {
+
+            computerUi.appendChild(scissorsIco);
             humanScore++;
             return "You won this round"
         }
+
+        computerUi.appendChild(paperIco);
         computerScore++;
         return "You lost this round"
     }
 
     if (humanChoice == "paper") {
+
+        humanUi.appendChild(paperIco);
+
         if (computerChoice == "rock") {
+            computerUi.appendChild(rockIco);
             humanScore++;
             return "You won this round"
         }
+
+        computerUi.appendChild(scissorsIco);
         computerScore++;
         return "You lost this round"
     }
 
     if (humanChoice == "scissors") {
+
+        humanUi.appendChild(scissorsIco);
+
         if (computerChoice == "paper") {
+            computerUi.appendChild(paperIco);
             humanScore++;
             return "You won this round"
         }
+
+        computerUi.appendChild(rockIco);
         computerScore++;
         return "You lost this round"
     }
-
-
-
-
-
 }
 
 
